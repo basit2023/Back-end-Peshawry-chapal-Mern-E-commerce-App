fe250fc935db723db9d4dc4ca53330c757b631e1
basit2023 <127020924+basit2023@users.noreply.github.com>
2023-11-18 13:31:06 +0500

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,12 +1,30 @@
-import JWT from "jsonwebtoken"
+import jwt from "jsonwebtoken"
 import userModel from './../models/userModel.js';
 
 //Protected Routes token base
 export const requireSignIn = async (req, res, next) => {
+  
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        error: "You are not logged in! Kindly logged in first",
+      });
+    }
+
+    
   try {
     
-    const decode = JWT.verify(
-      req.headers.authorization,
+    const decode = jwt.verify(
+      token,
       process.env.JWT_SECRET
     );
     req.user = decode;
